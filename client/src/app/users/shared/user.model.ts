6ed19e0dd857104656a10756096942cd41b97392
Jesper Hellström <jeshe316@student.liu.se>
2018-11-15 18:16:48 +0100

    Added attributes to the user interface. These attributes will be used on the profile page.

--- client/src/app/users/shared/user.model.ts
+++ client/src/app/users/shared/user.model.ts
@@ -1,4 +1,13 @@
 export interface User {
   id?: string;
   liu_id: string;
+  allergies: string;
+  current_grade: number;
+  class: string;
+  zip_code?: string;
+  address?: string;
+  city?: string;
+  master?: string;
+  bachelor?: string;
+  newspaper: boolean;
 }
