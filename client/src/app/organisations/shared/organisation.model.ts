7b1b99316184c4bb799f5d1ef251e2a73525baaf
Anton <webmaster@isektionen.se>
2018-11-15 19:01:45 +0100

    Added preliminary model files for users + organisations.

--- client/src/app/organisations/shared/organisation.model.ts
+++ client/src/app/organisations/shared/organisation.model.ts
@@ -1,4 +1,15 @@
 export interface Organisation {
   id?: string;
+
+  // Core
   name: string;
+  leader: string;
+
+  // Elements
+  description: string;
+  iconUrl: string;
+  imageUrl: string;
+
+  // Traceability
+  modified: string; // DATE/TIME + USER
 }
