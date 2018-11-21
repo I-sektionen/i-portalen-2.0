568bb70a2b7d7dc76c70a2c2c224e2b924b91130
Jesper Hellström <jeshe316@student.liu.se>
2018-11-21 19:26:03 +0100

    The values from firebase now shows in some of the fields

--- client/src/app/users/user-profile/user-profile.component.ts
+++ client/src/app/users/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { Observable } from 'rxjs/index';
 import { User } from '../shared/user.model';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 
 
@@ -13,13 +14,40 @@ import { User } from '../shared/user.model';
 export class UserProfileComponent implements OnInit {
 
   userObservable: Observable<User>;
+  user:User;
+  userInformation: FormGroup;
+
   constructor(
     private userService: UserService,
-  ) { }
-
-  ngOnInit() {
-    this.userObservable = this.userService.user;
+    private fb: FormBuilder,
+  ) {
+    this.userInformation = fb.group({
+      'address':[''],
+      'city':[''],
+      'zip_code':[''],
+      'allergies':[''],
+      'newspaper':[false],
+    });
   }
 
+  ngOnInit() {
+    //this.userObservable = this.userService.user;
 
+    this.userService.user.subscribe(user =>
+    {
+
+      this.user = user;
+      /*let keys = Object.keys(user);
+     keys.forEach(attribute =>
+     {
+       this.userInformation.addControl(attribute, user[attribute]);
+     })
+*/
+      this.userInformation.patchValue({'address': user.address});
+      this.userInformation.patchValue({'newspaper': user.newspaper});
+      this.userInformation.patchValue({'city': user.city});
+      this.userInformation.patchValue({'zip_code': user.zip_code});
+      this.userInformation.patchValue({'allergies': user.allergies});
+    })
+  }
 }
