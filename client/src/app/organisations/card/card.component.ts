ce1b23299ca0e3fd72e8d0ccadfcaa7234fa5991
Erik Ståhl <e.stahl95@gmail.com>
2018-11-27 23:48:59 +0100

    Added pictures to organisations

--- client/src/app/organisations/card/card.component.ts
+++ client/src/app/organisations/card/card.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input()
+  orgLogoUrl: string;
+
+  @Input()
   orgName: string;
 
   constructor() { }
