7a4522314260a36785c69f72b12cf7c6128e696c
Jacob <jacobkarlen99@gmail.com>
2018-12-11 17:26:17 +0100

    Merge branch 'Userpage-branch' of C:\Users\Jacob\Desktop\webgroup\angular\i-portalen-2.0 with conflicts.

--- client/src/app/core/nav/nav.component.ts
+++ client/src/app/core/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
--- client/src/app/users/user-login/user-login.component.ts
+++ client/src/app/users/user-login/user-login.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../../core/auth/auth.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss']
 })
+
+
 export class UserLoginComponent {
+
+  showPasswordField: boolean = true;
 
   loginForm = new FormGroup({
     liu_id: new FormControl('',
@@ -19,7 +23,9 @@ export class UserLoginComponent {
   });
 
   constructor(private authService: AuthService) {
+    this.onChanges();
   }
+
 
   get liu_id() {
     return this.loginForm.get('liu_id');
@@ -32,4 +38,11 @@ export class UserLoginComponent {
   login() {
     this.authService.loginLiUIDAndPassword(this.liu_id.value, this.password.value);
   }
+  onChanges(): void {
+this.loginForm.valueChanges.subscribe(val => {
+    this.wrongCred = false
+  });
 }
+}
+
+
