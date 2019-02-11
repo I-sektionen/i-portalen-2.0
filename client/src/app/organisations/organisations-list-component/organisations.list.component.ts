6b86d28f5d90f1eaed762be6fe059d80288cdaf7
Erik Ståhl <e.stahl95@gmail.com>
2019-02-11 19:17:06 +0100

    organisation list now fetches from firebase. TODO: Fix routing based on firebase organisation name

--- client/src/app/organisations/organisations-list-component/organisations.list.component.ts
+++ client/src/app/organisations/organisations-list-component/organisations.list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrganisationService } from '../shared/organisation.service';
+import { Observable } from 'rxjs';
+import { Organisation } from '../shared/organisation.model';
 
 @Component({
   selector: 'app-organisations',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganisationsListComponent implements OnInit {
   //@input
-  organisations = [
+  /* organisations = [
     { organisation: "webgroup", org_logo_url: "https://pbs.twimg.com/profile_images/500210368396816385/HzdAVBGq_400x400.png" },
     { organisation: "alumni", org_logo_url: "https://www.hv71.se/team_graphics/nef_shl/hv711-hv71-32ce9/logo_square/200.png" },
     { organisation: "näringslivsutskottet", org_logo_url: "https://pbs.twimg.com/profile_images/466315238/Farjestad_BK_twitter_400x400.gif" },
@@ -38,11 +41,15 @@ export class OrganisationsListComponent implements OnInit {
     { organisation: "näringslivsutskottet", org_logo_url: "https://pbs.twimg.com/profile_images/466315238/Farjestad_BK_twitter_400x400.gif" },
     { organisation: "styret", org_logo_url: "https://www.frolundaindians.com/team_graphics/nef_shl/fhc1-fhc-f7c59/logo_square/200.png" },
     { organisation: "clubmästeriet", org_logo_url: "https://www.roglebk.se/team_graphics/nef_shl/rbk1-rbk-539f1/logo_square/200.png" }
-  ]; //Här ska info hämtas från db istället
+  ]; */ //Här ska info hämtas från db istället
 
+  organisations: Observable<Organisation[]>;
 
-  constructor() { }
+  constructor(
+    private organisationService: OrganisationService
+  ) { }
 
   ngOnInit() {
+    this.organisations = this.organisationService.listOrganisations()
   }
 }
