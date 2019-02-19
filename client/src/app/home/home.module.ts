06ba97916defc2d662064cfd945e364b2080ca08
antmo646 <anton9393@gmail.com>
2019-02-19 19:07:54 +0100

    added hero component

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeroComponent],
   exports: [HomeComponent],
   imports: [
     CommonModule,
     SharedModule,
     CoreModule,
+    MDBBootstrapModule.forRoot()
   ]
 })
 export class HomeModule { }
