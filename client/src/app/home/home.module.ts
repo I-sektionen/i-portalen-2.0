e0889d3c56c70e3f6751157186f7d9a4d4553bb3
Jacob <jacobkarlen99@gmail.com>
2019-02-25 19:40:07 +0100

    Updated card design in home component

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
+import { AppRoutingModule} from "../app-routing.module";
+import {
+  MatButtonModule,
+  MatCardModule, MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule, MatSelectModule,
+  MatSlideToggleModule
+} from "@angular/material";
+import {FlexModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [HomeComponent],
@@ -11,6 +20,17 @@ import { CoreModule } from '../core/core.module';
     CommonModule,
     SharedModule,
     CoreModule,
+    FlexModule,
+    AppRoutingModule,
+
+    // Material
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatDialogModule
   ]
 })
 export class HomeModule { }
