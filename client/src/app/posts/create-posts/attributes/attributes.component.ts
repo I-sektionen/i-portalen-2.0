17983cc9fc718dd0b82e8980119ffef7af424464
Erik Ståhl <e.stahl95@gmail.com>
2019-04-02 09:49:56 +0200

    Continued work on attributes in creating post

--- client/src/app/posts/create-posts/attributes/attributes.component.ts
+++ client/src/app/posts/create-posts/attributes/attributes.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-attributes",
-  templateUrl: "./attributes.component.html",
-  styleUrls: ["./attributes.component.scss"]
+  selector: 'app-attributes',
+  templateUrl: './attributes.component.html',
+  styleUrls: ['./attributes.component.scss']
 })
 export class AttributesComponent implements OnInit {
   title: string;
   eventType: boolean = false;
-  typeOne: string = "Artikel";
-  typeTwo: string = "Event";
+  typeOne: string = 'Artikel';
+  typeTwo: string = 'Event';
+  postTypeIsArticle: boolean = true;
+  ingress: string = '';
 
   constructor() {}
 
