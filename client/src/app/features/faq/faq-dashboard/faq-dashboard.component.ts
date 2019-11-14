21c0f8a195cd6d4e2fdc2413a8f80574770e59b9
Jonatan Harling <jonha497@student.liu.se>
2019-11-14 18:44:37 +0100

    Fixat mocklista till Faq

--- client/src/app/features/faq/faq-dashboard/faq-dashboard.component.ts
+++ client/src/app/features/faq/faq-dashboard/faq-dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {Faq} from '../shared/faq.model';
 
 @Component({
   selector: 'app-faq-dashboard',
   templateUrl: './faq-dashboard.component.html',
   styleUrls: ['./faq-dashboard.component.scss']
 })
+
 export class FaqDashboardComponent implements OnInit {
+  faq = Faq;
 
   constructor() { }
 
--- client/src/app/features/faq/shared/faq.model.ts
+++ client/src/app/features/faq/shared/faq.model.ts
@@ -2,3 +2,8 @@ export interface FaqQuestion {
   question: string;
   answer: string;
 }
+
+export const Faq: FaqQuestion[] = [
+  { question: 'Hur fan då?', answer: 'Dr Nice' },
+  {question: 'Tjäna?', answer: 'Dr Nice' }
+];
