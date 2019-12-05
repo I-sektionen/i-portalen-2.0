1226dbfd7dc57d97b917b3005347183aa3e1bc64
Gustaf <gustaf.lindgren@live.se>
2019-12-05 18:57:16 +0100

    Test

--- client/src/app/features/faq/faq-dashboard/questansw/questansw.component.ts
+++ client/src/app/features/faq/faq-dashboard/questansw/questansw.component.ts
@@ -5,8 +5,8 @@ import {Component, Input, OnDestroy, OnInit} from '@angular/core';
   templateUrl: './questansw.component.html',
   styleUrls: ['./questansw.component.scss']
 })
-export class QuestanswComponent implements OnInit, OnDestroy { //Finns det bättre sätt än använda onDestroy?
-  @Input() answer: String; //varför hämtar den inte in answer???
+export class QuestanswComponent implements OnInit { // Finns det bättre sätt än använda onDestroy?
+  @Input() answer: String; // varför hämtar den inte in answer???
   @Input() question: String;
     x = false;
   constructor() {
