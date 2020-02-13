3e43bb7d4178c6a169eea28708a60d0df6f2c8e2
Jonatan Harling <jonha497@student.liu.se>
2020-02-13 19:34:32 +0100

    när man lägger till en användare i auth skapas en användare men namn test i db

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+try {admin.initializeApp(functions.config().firebase);} catch(e) {}
+const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+/**
+ * Firebase function 1
+ * Triggered when a new user is created with Firebase Auth
+ * Writes something to Firestore
+ */
+export const firebaseFunction = functions.auth.user().onCreate(async user => {
+  if (user.email) {
+    console.log('New user with email:', user.email)
+    // save something to Firestore
+    db.collection('users/').add({name:"test"}).catch(err => console.error(err))
+  }
 });
