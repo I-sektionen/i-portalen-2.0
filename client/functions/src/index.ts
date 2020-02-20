e58ed5b5dce2ac2f8ce17c1e9f29cb60cfdf7bdc
Jonatan Harling <jonha497@student.liu.se>
2020-02-20 16:32:42 +0100

    inget error i firecloud

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -7,13 +7,14 @@ const db = admin.firestore();
 
 export const firebaseOnCreateArticle = functions.firestore.document('articles/{articleId}').onCreate((snapshot,context) => {
 addNotification("articles", snapshot.data()!.name, "events", "Ny artikel")
-          })
+  return null
+          });
 
 // adds a notification with to the users who follows follow.
-export function addNotification(follow: String, body: String, link: String, title: String) {
+function addNotification(follow: String, body: String, link: String, title: String) {
   db.collection('users/').where("follow", 'array-contains', follow).get().then(dataSnapshot => {
     dataSnapshot.forEach(data => {
       db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err))
     })
   }).catch(err => console.error(err))
-}
+};
