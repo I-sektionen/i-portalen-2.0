e0c2f558b16de15335a78abc4c7b254bec6a01df
Jonatan Harling <jonha497@student.liu.se>
2020-02-20 17:43:51 +0100

    test

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -4,17 +4,19 @@ import Timestamp = admin.firestore.Timestamp;
 
 try {admin.initializeApp(functions.config().firebase);} catch(e) {}
 const db = admin.firestore();
-
-export const firebaseOnCreateArticle = functions.firestore.document('articles/{articleId}').onCreate((snapshot,context) => {
-addNotification("articles", snapshot.data()!.name, "events", "Ny artikel")
-  return null
+const collections = {
+  users: 'user/' 
+};
+export const firebaseOnCreateArticle = functions.firestore.document('articles/{articleId}').onCreate( (snapshot,context) => {
+  addNotification("articles", snapshot.data()!.name, "events", "Ny artikel");
+  return null;
           });
 
 // adds a notification with to the users who follows follow.
-function addNotification(follow: String, body: String, link: String, title: String) {
-  db.collection('users/').where("follow", 'array-contains', follow).get().then(dataSnapshot => {
+const addNotification = (follow: string, body: string, link: string, title: string) => {
+  db.collection(collections.users).where("follow", 'array-contains', follow).get().then(dataSnapshot => {
     dataSnapshot.forEach(data => {
       db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err))
-    })
-  }).catch(err => console.error(err))
+    });
+  }).catch(err => console.error(err));
 };
