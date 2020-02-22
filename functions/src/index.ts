fe0094405ffd6956b3c84197ae4cdb467250522d
Jonatan Harling <jonha497@student.liu.se>
2020-02-22 12:27:27 +0100

    funktiner funkar, kvar är att fixa tidkollen och snygga till koden

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,22 +9,44 @@ const db = admin.firestore();
 //kolla varje timme
 //const publishDated: string[] = [];
 
-//New created post
-export const firebaseOnCreatePost = functions.firestore.document('posts/{postId}').onCreate((snapshot,context) => {
-    addNotification("notAccepted", snapshot.data()!.type, "events", "title")
+//New created post => notifications to admins
+export const firebaseOnCreateNotAcceptedPost = functions.firestore.document('notAcceptedPosts/{notAcceptedPostId}').onCreate((snapshot,context) => {
+    addNotificationToFollow("notAccepted", "test", "events", "Ny post att acceptera")
     return null});
 
-//Value in post is changed
-export const firebaseOnUpdatePost = functions.firestore.document('posts/{postId}').onUpdate((change, context) => {
-    addNotification("notAccepted", change.after.data()!.type, "events", "title")
+//New post is public => notifications to the once who follow the post organisation
+export const firebaseOnPublishedPost = functions.firestore.document('posts/{postId}').onCreate((snapshot,context) => {
+    addNotificationToFollow(snapshot.data()!.organisation, snapshot.data()!.type, "events", "ny post")
+    return null});
+
+//Denied status is changed
+export const firebaseOnUpdatePost = functions.firestore.document('notAcceptedPosts/{notAcceptedPostId}').onUpdate((change, context) => {
+    if(change.after.data()!.denied === false) {
+        addNotificationToFollow("notAccepted", "test", "events", "Ny post att acceptera")
+    } else {
+        addNotificationToUser("wilri858", "testBody", "events", "hoppasTitle")
+    }
 return null});
 
 
 // adds a notification with to the users who follows follow.
-function addNotification(follow: String, body: String, link: String, title: String) {
+function addNotificationToFollow(follow: String, body: String, link: String, title: String) {
   db.collection('users/').where("follow", 'array-contains', follow).get().then(dataSnapshot => {
     dataSnapshot.forEach(data => {
       db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err))
     })
   }).catch(err => console.error(err))
 };
+
+function addNotificationToUser(liuid: String, body: String, link: String, title: String) {
+    db.collection('users/').where("liuId", '==', liuid).get().then(dataSnapshot => {
+        dataSnapshot.forEach(data => {
+            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err))
+        })
+    }).catch(err => console.error(err))
+};
+
+
+
+//ska lyssna på ändringar i public => notifikation till de som förljer
+//ska lyssna på ändringar i denied => notifikation för den som publicerade/de som kan acceptera
