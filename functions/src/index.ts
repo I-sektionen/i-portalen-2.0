776396f4bf98b106e5e5b122a42df3ca8f26e4f3
Jonatan Harling <jonha497@student.liu.se>
2020-02-23 02:00:12 +0100

    create är allmän

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,26 +5,20 @@ import {addNotificationToFollow, addNotificationToUser, paths, notificationGroup
 try {admin.initializeApp(functions.config().firebase);} catch(e) {}
 const db = admin.firestore();
 
-const onCreate = {createPath: "notAcceptedPosts", notificationGroupPath: 'notificationGroups/', notificationGroupType: 'type', createType: 'type', follow: 'followNotAccepted', notificationGroupBody: 'notAcceptedBody', notificationGroupLink: 'link', title: 'organisation' }
+const onCreateArray = [{createPath: "posts", notificationGroupPath: 'notificationGroups/', notificationGroupType: 'type', createType: 'type', follow: 'organisation', notificationGroupBody: 'newPostBody', notificationGroupLink: 'link', title: 'organisation' },
+    {createPath: "notAcceptedPosts", notificationGroupPath: 'notificationGroups/', notificationGroupType: 'type', createType: 'type', follow: 'followNotAccepted', notificationGroupBody: 'notAcceptedBody', notificationGroupLink: 'link', title: 'organisation' }]
 
 //New created post => notifications to admins
 export const firebaseOnCreateNotAcceptedPost = functions.firestore.document('/{collection}/{id}').onCreate((snapshot, context) => {
-    if(context.params.collection == onCreate.createPath) {
-        db.collection(onCreate.notificationGroupPath).where(onCreate.notificationGroupType, '==', snapshot.get(onCreate.createType)).get().then(dataSnapshot => {
-            dataSnapshot.forEach(data => {
-                addNotificationToFollow(snapshot.get(onCreate.follow), data.get(onCreate.notificationGroupBody), data.get(onCreate.notificationGroupLink), snapshot.get(onCreate.title));
-            });
-        }).catch(err => console.error(err))
-    }
-    return null});
-
-//New post is public => notifications to the once who follow the post organisation
-export const firebaseOnPublishedPost = functions.firestore.document(paths.posts).onCreate((snapshot,context) => {
-    db.collection(paths.notificationGroup).where(notificationGroupType, '==', snapshot.data()!.type).get().then(dataSnapshot => {
-        dataSnapshot.forEach(data => {
-    addNotificationToFollow(snapshot.data()!.organisation, data.data().newPostBody, data.data().link,  snapshot.data()!.organisation);
-        });
-    }).catch(err => console.error(err))
+   for (const onCreate of onCreateArray) {
+       if (context.params.collection == onCreate.createPath) {
+           db.collection(onCreate.notificationGroupPath).where(onCreate.notificationGroupType, '==', snapshot.get(onCreate.createType)).get().then(dataSnapshot => {
+               dataSnapshot.forEach(data => {
+                   addNotificationToFollow(snapshot.get(onCreate.follow), data.get(onCreate.notificationGroupBody), data.get(onCreate.notificationGroupLink), snapshot.get(onCreate.title));
+               });
+           }).catch(err => console.error(err))
+       }
+   }
     return null});
 
 //Denied status is changed
