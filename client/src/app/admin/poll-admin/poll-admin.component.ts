d0041ea59d58b37a1274181a800a58d651173bce
Albin Janzén <albinjanzen@gmail.com>
2020-02-25 12:54:01 +0100

    renaming and admin draft

---
+++ client/src/app/admin/poll-admin/poll-admin.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Poll} from '../../votings/shared/poll.model';
+import {PollService} from '../../votings/shared/poll.service';
+
+@Component({
+  selector: 'app-poll-admin',
+  templateUrl: './poll-admin.component.html',
+  styleUrls: ['./poll-admin.component.scss']
+})
+export class PollAdminComponent implements OnInit {
+  private pollList: Observable<Poll[]>;
+
+  constructor(private pollService: PollService) {
+    this.pollList = this.pollService.listPolls();
+
+  }
+
+  ngOnInit() {
+  }
+
+}
