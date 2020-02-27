87cc55c46ea3478a4abcec6e2cf802973c476bb0
Anton Nordin <antno521@student.liu.se>
2020-02-27 19:28:05 +0100

    get post

--- client/src/app/features/posts/shared/post.service.ts
+++ client/src/app/features/posts/shared/post.service.ts
@@ -1,10 +1,21 @@
 import {Injectable} from '@angular/core';
+import {DatabaseService} from '../../../core/database/database.service';
+import {Post} from './post.model';
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-
-  constructor() {
+  private readonly path = 'posts';
+  constructor(private databaseService: DatabaseService<Post>) {
+  }
+  getPost(id) {
+    return this.databaseService.doc(this.path, id).get().pipe(map((post => {
+      const data = post.data() as Post;
+      return {...data, id: post.id};
+    })));
   }
 }
+
+
