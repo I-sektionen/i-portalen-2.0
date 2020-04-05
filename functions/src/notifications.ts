ad0cb21beff1e82713718cde2a49e55e93475050
Jonatan Harling <jonha497@student.liu.se>
2020-04-05 09:56:29 +0200

    notifikationsklockan ringer vidny notis

--- functions/src/notifications.ts
+++ functions/src/notifications.ts
@@ -7,7 +7,13 @@ const db = admin.firestore();
 export function addNotificationToFollow(follow: String, body: String, link: String, title: String) {
     db.collection('users/').where("follow", 'array-contains', follow).get().then(dataSnapshot => {
         dataSnapshot.forEach(data => {
-            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err))
+            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err));
+            const ref = db.collection('users/').doc(data.id);
+                ref.get().then(data2 =>{
+                if(data2.exists){
+                    ref.update({newNotifications: true}).catch(err => console.error(err))
+                }
+            }).catch(err => console.error(err))
         })
     }).catch(err => console.error(err))
 };
