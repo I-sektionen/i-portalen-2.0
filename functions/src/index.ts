789fc65ce154c0abb08882ff5d639b316fcc3914
Jonatan Harling <jonha497@student.liu.se>
2020-04-08 22:04:10 +0200

    slutar funka ibland hoppas inte den gör det nu

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
-import {addNotificationToFollow} from "./notifications";
+import {addNotificationToFollow, addNotificationToUser} from "./notifications";
 //posts, not accepted posts, denied post
 try {admin.initializeApp(functions.config().firebase);} catch(e) {}
 const db = admin.firestore();
 
-interface Notification {
+interface OnCreateNotification {
     title: string;
     body: string;
     link: string;
@@ -22,12 +22,12 @@ export const onCreate1 = functions.firestore.document('/{collectionFirst}/{idFir
         for(const category of categories) {
             for (const notification of dataSnapshot.get(category)) {
                 const json = JSON.stringify(notification);
-                const notificationObject: Notification = JSON.parse(json);
+                const notificationObject: OnCreateNotification = JSON.parse(json);
                 if (notificationObject.collectionFirst === context.params.collectionFirst) {
                     if (notificationObject.fieldConstraint === "") {
-                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title)
+                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
                     } else if (snapshot.get(notificationObject.fieldConstraint) == notificationObject.valueConstraint) {
-                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title)
+                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
                     }
                 }
             }
@@ -41,15 +41,61 @@ export const onCreate2 = functions.firestore.document('/{collectionFirst}/{idFir
         for(const category of categories) {
             for (const notification of dataSnapshot.get(category)) {
                 const json = JSON.stringify(notification);
-                const notificationObject: Notification = JSON.parse(json);
+                const notificationObject: OnCreateNotification = JSON.parse(json);
                 if (((notificationObject.collectionFirst === context.params.collectionFirst) && (notificationObject.collectionSecond === context.params.collectionSecond)) && (notificationObject.id === context.params.idFirst)) {
                     if (notificationObject.fieldConstraint === "") {
-                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title)
+                       addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
                     } else if (snapshot.get(notificationObject.fieldConstraint) == notificationObject.valueConstraint) {
-                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title)
+                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
                     }
                 }
             }
         }
     }).catch(err => console.error(err))
     return null});
+
+interface onCreateIndividualNotification {
+    title: string;
+    body: string;
+    link: string;
+    collectionFirst: string;
+    collectionSecond: string;
+    id: string;
+    fieldConstraint: string;
+}
+
+export const onCreateIndividual1 = functions.firestore.document('/{collectionFirst}/{idFirst}').onCreate((snapshot, context) => {
+    db.doc('notifications/onCreateIndividual').get().then(dataSnapshot => {
+        const categories = Object.keys(dataSnapshot.data()!);
+        for(const category of categories) {
+            for (const notification of dataSnapshot.get(category)) {
+                const json = JSON.stringify(notification);
+                const notificationObject: onCreateIndividualNotification = JSON.parse(json);
+                console.log(notificationObject);
+                if (notificationObject.collectionFirst === context.params.collectionFirst) {
+                    addNotificationToUser(snapshot.get(String(notificationObject.fieldConstraint)), notificationObject.body, notificationObject.link, notificationObject.title, db)
+                }
+            }
+        }
+    }).catch(err => console.error(err))
+    return null});
+
+export const onCreateIndividual2 = functions.firestore.document('/{collectionFirst}/{idFirst}/{collectionSecond}/{idSecond}').onCreate((snapshot, context) => {
+    db.doc('notifications/onCreateIndividual').get().then(dataSnapshot => {
+        const categories = Object.keys(dataSnapshot.data()!);
+        console.log(categories);
+        for(const category of categories) {
+            console.log(dataSnapshot.get(category));
+            for (const notification of dataSnapshot.get(category)) {
+                const json = JSON.stringify(notification);
+                console.log(json);
+                const notificationObject: onCreateIndividualNotification = JSON.parse(json);
+                console.log(notificationObject);
+                if (((notificationObject.collectionFirst === context.params.collectionFirst) && (notificationObject.id === context.params.idFirst)) && (notificationObject.collectionSecond === String(context.params.collectionSecond))) {
+                    addNotificationToUser(snapshot.get(String(notificationObject.fieldConstraint)), notificationObject.body, notificationObject.link, notificationObject.title, db)
+                }
+            }
+        }
+    }).catch(err => console.error(err))
+    return null});
+
--- functions/src/notifications.ts
+++ functions/src/notifications.ts
@@ -1,10 +1,7 @@
 import * as admin from "firebase-admin";
 import Timestamp = admin.firestore.Timestamp;
-import * as functions from "firebase-functions";
-try {admin.initializeApp(functions.config().firebase);} catch(e) {}
-const db = admin.firestore();
 
-export function addNotificationToFollow(follow: String, body: String, link: String, title: String) {
+export function addNotificationToFollow(follow: String, body: String, link: String, title: String, db:FirebaseFirestore.Firestore ) {
     db.collection('users/').where("follow", 'array-contains', follow).get().then(dataSnapshot => {
         dataSnapshot.forEach(data => {
             db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err));
@@ -16,15 +13,21 @@ export function addNotificationToFollow(follow: String, body: String, link: Stri
             }).catch(err => console.error(err))
         })
     }).catch(err => console.error(err))
-};
+}
 
-export function addNotificationToUser(liuid: String, body: String, link: String, title: String) {
+export function addNotificationToUser(liuid: String, body: String, link: String, title: String, db:FirebaseFirestore.Firestore ) {
     db.collection('users/').where("liuId", '==', liuid).get().then(dataSnapshot => {
         dataSnapshot.forEach(data => {
-            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err))
+            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err));
+            const ref = db.collection('users/').doc(data.id);
+            ref.get().then(data2 =>{
+                if(data2.exists){
+                    ref.update({newNotifications: true}).catch(err => console.error(err))
+                }
+            }).catch(err => console.error(err))
         })
     }).catch(err => console.error(err))
-};
+}
 
 
 
