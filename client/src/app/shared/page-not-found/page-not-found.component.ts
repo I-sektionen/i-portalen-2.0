1b83a83b6cd8c28801619a20878d14409f79e5c3
williamrimton <william@rimton.se>
2020-04-09 17:01:17 +0200

    test

--- client/src/app/shared/page-not-found/page-not-found.component.ts
+++ client/src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private.Router: Router,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(
+    this.Router.url;
+  ) {
   }
 
 }
