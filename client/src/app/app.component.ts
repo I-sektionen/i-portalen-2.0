fa2b61e0394a9b43a4c5fd4c59162c3170abfcdb
albinjal <albin.jaldevik@gmail.com>
2020-04-09 16:43:07 +0200

    hotfix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {PostService} from './features/posts/shared/post.service';
-import {PostStatus} from './features/posts/shared/post-status.enum';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +6,8 @@ import {PostStatus} from './features/posts/shared/post-status.enum';
 })
 
 export class AppComponent implements OnInit {
-  constructor(private postServ: PostService) {
+  constructor() {
   }
   ngOnInit(): void {
-    this.postServ.list(PostStatus.ApprovedWaiting).subscribe(value => console.log(value));
   }
 }
