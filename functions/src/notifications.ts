8abdd7c2d71bdd7f292ebb5a2c9fa0246cd644e5
Jonatan Harling <jonha497@student.liu.se>
2020-04-27 16:54:15 +0200

    gjort koden mer lättläst

--- functions/src/notifications.ts
+++ functions/src/notifications.ts
@@ -27,7 +27,10 @@ interface onCreateIndividualNotification {
  function addNotificationToFollow(follow: string, body: string, link: string, title: string, db:FirebaseFirestore.Firestore ) {
     db.collection('users/').where("follow", 'array-contains', follow).get().then(dataSnapshot => {
         dataSnapshot.forEach(data => {
-            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err));
+            db.collection('users/').doc(data.id)
+                .collection('notifications')
+                .add({body: body, link: link, timestamp: Timestamp.now(), title:title})
+                .catch(err => console.error(err));
             const ref = db.collection('users/').doc(data.id);
                 ref.get().then(data2 =>{
                 if(data2.exists){
@@ -39,9 +42,14 @@ interface onCreateIndividualNotification {
 }
 
  function addNotificationToUser(liuid: string, body: string, link: string, title: string, db:FirebaseFirestore.Firestore ) {
-    db.collection('users/').where("liuId", '==', liuid).get().then(dataSnapshot => {
+    db.collection('users/')
+        .where("liuId", '==', liuid)
+        .get().then(dataSnapshot => {
         dataSnapshot.forEach(data => {
-            db.collection('users/').doc(data.id).collection('notifications').add({body: body, link: link, timestamp: Timestamp.now(), title:title}).catch(err => console.error(err));
+            db.collection('users/').doc(data.id)
+                .collection('notifications')
+                .add({body: body, link: link, timestamp: Timestamp.now(), title:title})
+                .catch(err => console.error(err));
             const ref = db.collection('users/').doc(data.id);
             ref.get().then(data2 =>{
                 if(data2.exists){
@@ -51,7 +59,9 @@ interface onCreateIndividualNotification {
         })
     }).catch(err => console.error(err))
 }
+
 export function onCreateFunction1(snapshot: DocumentSnapshot, context: EventContext) {
+    // onCreate follow
     const db = admin.firestore();
     db.doc('notifications/onCreate').get().then(dataSnapshot => {
         const categories = Object.keys(dataSnapshot.data()!);
@@ -61,14 +71,23 @@ export function onCreateFunction1(snapshot: DocumentSnapshot, context: EventCont
                 const notificationObject: OnCreateNotification = JSON.parse(json);
                 if (notificationObject.collectionFirst === context.params.collectionFirst) {
                        if (notificationObject.fieldConstraint === "") {
-                            addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
+                            addNotificationToFollow(notificationObject.follow,
+                                notificationObject.body, notificationObject.link,
+                                notificationObject.title,
+                                db)
                        } else if (snapshot.get(notificationObject.fieldConstraint) === notificationObject.valueConstraint) {
-                            addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
+                            addNotificationToFollow(notificationObject.follow,
+                                notificationObject.body,
+                                notificationObject.link,
+                                notificationObject.title,
+                                db)
                      }
                         }
             }
         }
     }).catch(err => console.error(err));
+
+    // onCreate individual
     db.doc('notifications/onCreateIndividual').get().then(dataSnapshot => {
         const categories = Object.keys(dataSnapshot.data()!);
         for(const category of categories) {
@@ -77,7 +96,11 @@ export function onCreateFunction1(snapshot: DocumentSnapshot, context: EventCont
                 const notificationObject: onCreateIndividualNotification = JSON.parse(json);
                 console.log(notificationObject);
                 if (notificationObject.collectionFirst === context.params.collectionFirst) {
-                    addNotificationToUser(snapshot.get(String(notificationObject.fieldConstraint)), notificationObject.body, notificationObject.link, notificationObject.title, db)
+                    addNotificationToUser(snapshot.get(String(notificationObject.fieldConstraint)),
+                        notificationObject.body,
+                        notificationObject.link,
+                        notificationObject.title,
+                        db)
                 }
             }
         }
@@ -85,6 +108,7 @@ export function onCreateFunction1(snapshot: DocumentSnapshot, context: EventCont
 }
 
 export function onCreateFunction2(snapshot: DocumentSnapshot, context: EventContext) {
+    // onCreate follow
     const db = admin.firestore();
     db.doc('notifications/onCreate').get().then(dataSnapshot => {
         const categories = Object.keys(dataSnapshot.data()!);
@@ -92,16 +116,28 @@ export function onCreateFunction2(snapshot: DocumentSnapshot, context: EventCont
             for (const notification of dataSnapshot.get(category)) {
                 const json = JSON.stringify(notification);
                 const notificationObject: OnCreateNotification = JSON.parse(json);
-                if (((notificationObject.collectionFirst === context.params.collectionFirst) && (notificationObject.collectionSecond === context.params.collectionSecond)) && (notificationObject.id === context.params.idFirst)) {
+                if (((notificationObject.collectionFirst === context.params.collectionFirst) &&
+                    (notificationObject.collectionSecond === context.params.collectionSecond)) &&
+                    (notificationObject.id === context.params.idFirst)) {
                     if (notificationObject.fieldConstraint === "") {
-                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
+                        addNotificationToFollow(notificationObject.follow,
+                            notificationObject.body,
+                            notificationObject.link,
+                            notificationObject.title,
+                            db)
                     } else if (snapshot.get(notificationObject.fieldConstraint) === notificationObject.valueConstraint) {
-                        addNotificationToFollow(notificationObject.follow, notificationObject.body, notificationObject.link, notificationObject.title, db)
+                        addNotificationToFollow(notificationObject.follow,
+                            notificationObject.body,
+                            notificationObject.link,
+                            notificationObject.title,
+                            db)
                     }
                 }
             }
         }
     }).catch(err => console.error(err));
+
+    //onCreate individual
     db.doc('notifications/onCreateIndividual').get().then(dataSnapshot => {
         const categories = Object.keys(dataSnapshot.data()!);
         console.log(categories);
@@ -112,8 +148,14 @@ export function onCreateFunction2(snapshot: DocumentSnapshot, context: EventCont
                 console.log(json);
                 const notificationObject: onCreateIndividualNotification = JSON.parse(json);
                 console.log(notificationObject);
-                if (((notificationObject.collectionFirst === context.params.collectionFirst) && (notificationObject.id === context.params.idFirst)) && (notificationObject.collectionSecond === String(context.params.collectionSecond))) {
-                    addNotificationToUser(snapshot.get(String(notificationObject.fieldConstraint)), notificationObject.body, notificationObject.link, notificationObject.title, db)
+                if (((notificationObject.collectionFirst === context.params.collectionFirst) &&
+                    (notificationObject.id === context.params.idFirst)) &&
+                    (notificationObject.collectionSecond === String(context.params.collectionSecond))) {
+                    addNotificationToUser(snapshot.get(String(notificationObject.fieldConstraint)),
+                        notificationObject.body,
+                        notificationObject.link,
+                        notificationObject.title,
+                        db)
                 }
             }
         }
